bfc2fc53d72e4eef04416434b20745414d052e52
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-26 18:15:25 +0530

    upload pdf to disk

--- projects/fastapi-pdf-rag-api/app/main.py
+++ projects/fastapi-pdf-rag-api/app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,File, UploadFile
+import shutil
+from pathlib import Path
+from uuid import uuid4
 
 app = FastAPI()
 
@@ -6,3 +9,15 @@ app = FastAPI()
 async def read_root():
     return {"message": "okay"}
 
+@app.post("/upload-pdf")
+async def upload_pdf(file: UploadFile = File(...)):
+    stored_filename = f"{uuid4()}{Path(file.filename).suffix}"
+
+    with open(f"uploaded_files/{stored_filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    return {
+        "filename": stored_filename,
+        "message": "PDF uploaded successfully"  ,
+        "content_type": file.content_type
+        }
